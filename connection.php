51caab280a65218744348de4677e3341d66baac3
MKH-MK <khametov.m@oundleschool.org.uk>
2025-05-16 11:14:38 +0100

    Small changes for testing and cleaning.

--- connection.php
+++ connection.php
@@ -7,6 +7,6 @@ $dbname = "OUNSwimTeam";
 
 $conn = new PDO("mysql:host=$servername; dbname=$dbname", $username, $password);
 $conn -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-#echo("connected!!");
+echo("DB Connected");
 
 ?>
--- install.php
+++ install.php
@@ -1,12 +1,8 @@
-<?php
+<?php 
 
-$servername = "localhost";
-$username = "root";
-$password = "";
+include_once("connection.php");
 
 try {
-    $conn = new PDO("mysql:host=$servername", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = "CREATE DATABASE IF NOT EXISTS OUNSwimTeam";
     $conn->exec($sql);
     
@@ -19,6 +15,6 @@ catch(PDOException $e)
 {
     echo $sql . "<br>" . $e->getMessage();
 }
-$conn=Null;
+$conn = null;
 
 ?>
